98afeead3222e1fda52e573948ae487abcdb5d0b
Katlin Denney <kdenney@byupathway.edu>
2025-03-13 15:00:14 -0600

    js courses. havent found my error

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
 function displayCourses(filter = "All"){
+    console.log(`Displaying courses for: ${filter}`);
     const container = document.getElementById("courseContainer");
+    if (!container) {
+        console.error("courseContainer div NOT found!");
+        return;}
     //clear content in the courseContainer
     container.innerHTML = "";
 
     let filteredCourses = courses;
-    if (filter !== "ALL") {
+    if (filter.toUpperCaase() !== "ALL") {
         filteredCourses = courses.filter(course => course.subject == filter);
     }
+
     filteredCourses.forEach(course => {
         let courseDiv = document.createElement("div");
         courseDiv.classList.add("course-item", course.completed ? "completed" : "not-completed");
-        courseDiv.innerHTML = `<strong> $course.subject} ${course.number} - ${course.title}</strong>`;
-        container.appenedChild(courseDiv);
+        courseDiv.innerHTML = '<strong> ${course.subject}${course.number} - ${course.title}</strong>';
+        container.appendChild(courseDiv);
     });
 }
 document.getElementById("all").addEventListener("click", () => displayCourses("All"));
