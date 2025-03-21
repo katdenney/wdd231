8886a089cf9c02d316936d92bd7f3bdeadfbfed3
Katlin Denney <kdenney@byupathway.edu>
2025-03-20 19:16:13 -0600

    hamburger working!

--- scripts/course.js
+++ scripts/course.js
@@ -77,3 +77,35 @@ const courses = [
         completed: false
     }
 ]
+function displayCourses(filter = "All"){
+    console.log(`Displaying courses for: ${filter}`);
+    const container = document.getElementById("courseContainer");
+    if (!container) {
+        console.error("courseContainer div NOT found!");
+        return;}
+    //clear content in the courseContainer
+    container.innerHTML = "";
+
+    let filteredCourses = courses;
+    if (filter.toUpperCase() !== "ALL") {
+        filteredCourses = courses.filter(course => course.subject == filter);
+    }
+
+    console.log(`Filtered courses: `, filteredCourses);
+
+    filteredCourses.forEach(course => {
+        let courseDiv = document.createElement("div");
+        courseDiv.classList.add("course-item", course.completed ? "completed" : "not-completed");
+        courseDiv.innerHTML = `<strong> ${course.subject}${course.number} - ${course.title}</strong>`;
+        
+
+        console.log(`Adding course: ${course.subject} ${course.number}`);
+
+        container.appendChild(courseDiv);
+    });
+}
+document.getElementById("all").addEventListener("click", () => displayCourses("All"));
+document.getElementById("wdd").addEventListener("click", () => displayCourses("WDD"));
+document.getElementById("cse").addEventListener("click", () => displayCourses("CSE"));
+
+document.addEventListener("DOMContentLoaded", () => displayCourses());
--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //menu toggle hamburger
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-function displayCourses(filter = "All"){
-    console.log(`Displaying courses for: ${filter}`);
-    const container = document.getElementById("courseContainer");
-    if (!container) {
-        console.error("courseContainer div NOT found!");
-        return;}
-    //clear content in the courseContainer
-    container.innerHTML = "";
-
-    let filteredCourses = courses;
-    if (filter.toUpperCase() !== "ALL") {
-        filteredCourses = courses.filter(course => course.subject == filter);
-    }
-
-    console.log(`Filtered courses: `, filteredCourses);
-
-    filteredCourses.forEach(course => {
-        let courseDiv = document.createElement("div");
-        courseDiv.classList.add("course-item", course.completed ? "completed" : "not-completed");
-        courseDiv.innerHTML = `<strong> ${course.subject}${course.number} - ${course.title}</strong>`;
-        
-
-        console.log(`Adding course: ${course.subject} ${course.number}`);
-
-        container.appendChild(courseDiv);
+    menuToggle.addEventListener("click", function() {
+        menu.classList.toggle("open");
     });
-}
-document.getElementById("all").addEventListener("click", () => displayCourses("All"));
-document.getElementById("wdd").addEventListener("click", () => displayCourses("WDD"));
-document.getElementById("cse").addEventListener("click", () => displayCourses("CSE"));
-
-document.addEventListener("DOMContentLoaded", () => displayCourses());
+});
