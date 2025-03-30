c4594ee089452e94cfae072a642bc7743abd0cb9
Katlin Denney <kdenney@byupathway.edu>
2025-03-29 19:12:30 -0600

    directory good enough?

---
+++ chamber/scripts/date.js
@@ -0,0 +1,6 @@
+//date for footer
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#currentyear").textContent = new Date().getFullYear()
+    document.querySelector("#lastModified").textContent = "Last Modified: " + document.lastModified
+ });
+    
