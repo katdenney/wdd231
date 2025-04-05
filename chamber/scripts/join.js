b841efee93f9786436b9ed2aaf16ec25e47d8c37
Katlin Denney <kdenney@byupathway.edu>
2025-04-04 20:12:46 -0600

    combined the two event listeners

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,8 +2,5 @@ import { setupModals } from './modal.js';
 
 document.addEventListener('DOMContentLoaded', ()=> {
     setupModals();
-});
-import { getTimeStamp } from './timestamp.js';
-document.addEventListener('DOMContentLoaded', () => {
     getTimeStamp();
 });
